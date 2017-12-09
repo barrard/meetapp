19629ab44022658bde486898e05b45133950d090
David <barrard@gmail.com>
2017-12-08 19:22:53 -1000

    fixing sockets maybe

--- client/js/socket.js
+++ client/js/socket.js
@@ -1,7 +1,8 @@
-// var HOST = 66.8.168.178
+// var HOST = '66.8.168.178'
+var HOST = location.host
 var PROTOCOL = location.protocol
  // var socket = io(PROTOCOL+'//'+HOST);
- var socket = io(PROTOCOL+'//66.8.168.178/', {path:'/meetapp/socket.io'});
+ var socket = io(PROTOCOL+'//'+HOST, {path:'/meetapp/socket.io'});
  // var socket = io('https://draggon.us/meetapp');
 var socketid;
  socket.on('connection', function (data, myHtml) {
